b380a269273cfa1ac5778373077c60110770e8ed
Joshua Dibs <156900732+thejoshuadibs@users.noreply.github.com>
2025-12-10 23:02:00 +0100

    feat: Add procedures for gameplay and rounds
    Added the startGame and playRound functions.
    
    The playRound invokes each round while the
    startGame plays the five rounds of the game
    by calling playRound five times.

--- script.js
+++ script.js
@@ -1,27 +1,63 @@
+let playerScore = 0;
+let computerScore = 0;
+
 // Get User Choice
 function getUserChoice() {
-    const choice = prompt("Choose one: ROCK | PAPER | SCISSORS")
-    return choice.toLowerCase();
+  const choice = prompt("Choose one: ROCK | PAPER | SCISSORS");
+  return choice.toLowerCase();
 }
 
 // Get Computer Choice
 function getComputerChoice() {
-    const choice = ["Rock", "Paper", "Scissors"][Math.floor(Math.random() * 3)]
-    return choice.toLowerCase();
+  const choice = ["Rock", "Paper", "Scissors"][Math.floor(Math.random() * 3)];
+  return choice.toLowerCase();
 }
 
 // Computer Choices to Determine Winner
 function determineWinner(user, computer) {
-    if (user == computer) return "tie" 
-    if(user == "rock" && computer == "scissors" || user == "paper" && computer == "rock" || user == "scissors" && computer == "paper")
-        return "win"
-    return "lose"
+  if (user == computer) return "tie";
+  if (
+    (user == "rock" && computer == "scissors") ||
+    (user == "paper" && computer == "rock") ||
+    (user == "scissors" && computer == "paper")
+  ) {
+    playerScore += 1;
+    return "win";
+  }
+  computerScore += 1;
+  return "lose";
 }
 
 function displayResult(user, computer, result) {
-    if (result == "tie") 
-        console.log(`You: ${user} \nMo: ${computer} \nResult: It's a tie`)
-    else console.log(`You: ${user} \nMo: ${computer} \nResult: You ${result}`)
+  if (result == "tie")
+    console.log(`You: ${user} \nMo: ${computer} \nResult: It's a tie`);
+  else console.log(`You: ${user} \nMo: ${computer} \nResult: You ${result}`);
 }
 
+function playRound() {
+  const user = getUserChoice();
+  const computer = getComputerChoice();
+  const result = determineWinner(user, computer);
+  displayResult(user, computer, result);
+}
 
+function startGame() {
+  console.log(`\n------------------------------------------
+    \n⚔️ Welcome to Rock, Paper, Scissors: Console Clash! ⚔️
+\n------------------------------------------
+\nThis is a 5-round battle against Mo our pro bot.
+\n\nEnter 'rock', 'paper', or 'scissors' when prompted.
+\nLoading game engine...
+\nThe game will start momentarily. Prepare your move!
+\nGood luck, and may the best hand win!
+\n------------------------------------------`);
+
+  for (let i = 0; i < 5; i++) setTimeout(playRound, 1000);
+
+  console.log("Game Ended!");
+  console.log(
+    `Your Score: ${playerScore}\nMo's Score: ${computerScore}\n\nWinner: ${t}`
+  );
+}
+
+startGame();
